2a8ca131271941853c6a853718a8f7bca572d4e1
Korreq <lukasz2600@gmail.com>
2024-11-06 09:05:13 +0100

    Translation test

--- kanjiReader/main.py
+++ kanjiReader/main.py
@@ -1,3 +1,18 @@
+from transformers import AutoTokenizer, MarianMTModel
+
+def main():
+
+    model = MarianMTModel.from_pretrained("Helsinki-NLP/opus-mt-ja-en")
+    tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-ja-en")
+
+    text = "今日はいい天気ですね"
+
+    batch = tokenizer( [text], return_tensors="pt" )
+
+    generated_ids = model.generate( **batch )
+
+    print( tokenizer.batch_decode( generated_ids, skip_special_tokens=True )[0] )
+
 if __name__ == "__main__":
 
-    print( "O Hi Mark" )
+    main()
