30f68929373eba846ae0b52abb0fc9a15104cc1f
Korreq <lukasz2600@gmail.com>
2024-11-09 13:13:09 +0100

    OCR reader tests

--- kanjiReader/main.py
+++ kanjiReader/main.py
@@ -1,17 +1,42 @@
+#pip install transformers torch sentencepiece sacremoses
 from transformers import AutoTokenizer, MarianMTModel
+#pip install pillow
+from PIL import Image
+#pip install manga_ocr
+from manga_ocr import MangaOcr
 
-def main():
+
+def translate( text ):
 
     model = MarianMTModel.from_pretrained("Helsinki-NLP/opus-mt-ja-en")
     tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-ja-en")
-
-    text = "今日はいい天気ですね"
 
     batch = tokenizer( [text], return_tensors="pt" )
 
     generated_ids = model.generate( **batch )
 
-    print( tokenizer.batch_decode( generated_ids, skip_special_tokens=True )[0] )
+    return tokenizer.batch_decode( generated_ids, skip_special_tokens=True )[0]
+
+
+def textFromImage( img ):
+
+    mocr = MangaOcr()
+
+    return mocr( Image.open( img ).convert("RGB") )
+
+
+def main():
+
+    #text = "今日はいい天気ですね"
+
+    image = r"files/images/test.png"
+
+    textImg = textFromImage( image )
+
+    print( textImg )
+
+    print( translate( textImg ) )
+
 
 if __name__ == "__main__":
 
